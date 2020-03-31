53195c5e8c6be2901576c91b48b7e9bb27e80bd6
Jun <cho13234@ibksystem.co.kr>
2020-03-31 18:24:40 +0900

    Jenkinsfile upload

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,8 +1,79 @@
 node {
-    stage('example1') {
-        echo "is example1"
+	def useTest = true
+	def useBuild = true
+	def useDeploy = true
+	
+    stage('FlowCheck') {
+        try {
+	        println "  TEST FLOW = $USE_TEST"
+	        useTest = "$USE_TEST" == "true"
+	    }
+	    catch (MissingPropertyException e) {
+	        println "  TEST FLOW = true"
+	    }
+	
+	    try {
+	        println "  BUILD FLOW = $USE_BUILD"
+	        useBuild = "$USE_BUILD" == "true"
+	    }
+	    catch (MissingPropertyException e) {
+	        println "  BUILD FLOW = true"
+	    }
+	
+	    try {
+	        println "  DEPLOY FLOW = $USE_DEPLOY"
+	        useBuild = "$USE_DEPLOY" == "true"
+	    }
+	    catch (MissingPropertyException e) {
+	        println "  BUILD DEPLOY = true"
+	    }
+	
+	    try {
+	        println "  SWITCH FLOW = $USE_SWITCH"
+	        useBuild = "$USE_SWITCH" == "true"
+	    }
+	    catch (MissingPropertyException e) {
+	        println "  SWITCH FLOW = true"
+	    }
     }
-    stage('example2') {
-        echo "is example2"
+    stage('ParameterCheck') {
+        println "  JAVA_VERSION = $JAVA_VERSION"
+        println "  GIT_URL = $GIT_URL"
+    }
+    stage('Checkout') {
+    	if (useTest || useBuild) {
+    		println "Git Checkout Started"
+    		checkout(
+                [
+                        $class                           : 'GitSCM',
+                        branches                         : [[name: '${BRANCH_SELECTOR}']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions                       : [],
+                        submoduleCfg                     : [],
+                        userRemoteConfigs                : [[url: '${GIT_URL}']]
+                ]
+        	)
+        	println "Git CheckOut End"
+    	} else {
+	        println "Git CheckOut Skip"
+	    }
+    }
+    stage('Build') {
+    }
+    stage('Test') {
+    	if (useTest) {
+	        println "Test Started"
+	        println "Test End"
+	    } else {
+	        println "Test Skip"
+	    }
+    }
+    stage('Deploy') {
+    	if(useDeploy) {
+	        println "Deploy Started"
+	        println "Deploy End"
+	    } else {
+	        println "Deploy Skip"
+	    }
     }
 }
