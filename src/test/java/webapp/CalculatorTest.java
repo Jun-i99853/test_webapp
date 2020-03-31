e14a6bab2944bc1c9f69071e8be46282444a5edd
Jun <cho13234@ibksystem.co.kr>
2020-03-31 17:06:56 +0900

    add Junit Test Code

---
+++ src/test/java/webapp/CalculatorTest.java
@@ -0,0 +1,14 @@
+package webapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	public void testSum() {
+		Calculator cal = new Calculator();
+		double result = cal.sum(10, 100);
+		assertEquals(110, result, 0);
+	}
+}
